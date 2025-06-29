7f5141ebab6418ec252593609be4d00f4632a217
6amson <bunmigrey@icloud.com>
2025-06-29 10:30:11 +0100

    mini fix

--- app/graphql/mutations/create_complaint.rb
+++ app/graphql/mutations/create_complaint.rb
@@ -9,18 +9,19 @@ module Mutations
 
     def resolve(complaint_type:, complain:, attachment: nil)
       user = context[:current_user]
+      raise GraphQL::ExecutionError, "Authentication required" if user.nil?
       Rails.logger.info("EXCESSESS USER: #{user}")
       params = {
         complaint_type: complaint_type,
         user_id: user.id,
         complain: complain,
-        attachment: attachment
+        attachment: attachment,
       }
 
       result = ComplaintService.create_complaint(params)
       {
         success: result[:success],
-        complaint: result[:complaint]
+        complaint: result[:complaint],
       }
     rescue HttpError => e
       GraphQL::ExecutionError.new(e.message)
