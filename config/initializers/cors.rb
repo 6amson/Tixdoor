84e9ac9c2784889b8ec2cd630ec1840ab96da2e7
6amson <bunmigrey@icloud.com>
2025-06-29 01:09:12 +0100

    cors update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://tixdoor.vercel.app'
+    origins "https://tixdoor.vercel.app"
 
-    resource '*',
+    resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['Authorization']
+      expose: ["Authorization"]
   end
 end
